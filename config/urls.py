e656767184e93873f4e03929c9aafa70d297a707
Vika <vika.davlyatshina@mail.ru>
2026-05-17 13:03:08 +0200

    Добавлены маршруты приложений и поддержка медиа файлов

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/documents/', include('documents.urls', namespace='documents')),
+    path('api/users/', include('users.urls', namespace='users'))
 ]
+
+# Поддержка медиа-файлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
