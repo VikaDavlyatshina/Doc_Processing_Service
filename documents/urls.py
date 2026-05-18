4422f65bb6ab7f13738ab352629307f16a627369
Vika <vika.davlyatshina@mail.ru>
2026-05-18 18:03:17 +0200

    Добавлены новые маршруты. Для обновления документа, отправки док-ты, маршруты для подтверждения или отклонения документа

--- documents/urls.py
+++ documents/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .apps import DocumentsConfig
-from .views import DocumentViewSet
+from .views import DocumentViewSet, DocumentOwnerViewsSet, DocumentModerationViewSet
 
 app_name = DocumentsConfig.name
 
@@ -15,11 +15,21 @@ router.register(r"", DocumentViewSet, basename="documents")
 # GET    /documents/        → список
 # POST   /documents/        → создать
 # GET    /documents/{id}/   → просмотр
-# PUT    /documents/{id}/   → обновить
-# PATCH  /documents/{id}/   → частично обновить
 # DELETE /documents/{id}/   → удалить
+# POST    /documents/{id}/update-file/   → обновить документ
+# POST  /documents/{id}/submit/   → отправить черновик документа на проверку
+# POST  /documents/{id}/approve/   → подтвердить документ
+# POST  /documents/{id}/reject/   → отклонить документ
 
 urlpatterns = [
     # Все URL от router (CRUD для документа)
     path("", include(router.urls)),
+    # Обновление документа владельцем
+    path("<int:pk>/update-file/", DocumentOwnerViewsSet.as_view({'post': 'update_file'})),
+    # Отправка черновика документа на проверку модератору
+    path("<int:pk>/submit/", DocumentOwnerViewsSet.as_view({'post': 'submit'})),
+    # Подтверждение документа модератором
+    path("<int:pk>/approve/", DocumentModerationViewSet.as_view({'post': 'approve'})),
+    # Отклонение документа модератором
+    path("<int:pk>/reject/", DocumentModerationViewSet.as_view({'post': 'reject'})),
 ]
